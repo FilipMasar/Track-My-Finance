24fce83fad3c486c4e8547c8f8275acfc432f32f
Filip Masar <filip.masar99@gmail.com>
2018-08-01 17:15:01 +0200

    Managing incomes

--- Track My Finance/IncomeViewController.swift
+++ Track My Finance/IncomeViewController.swift
@@ -8,14 +8,56 @@
 
 import UIKit
 
-class IncomeViewController: UIViewController {
+class IncomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    var incomes = [[String:String]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        table.dataSource = self
+        table.delegate = self
+        
+        table.rowHeight = 50
+        
+        if let temp = UserDefaults.standard.object(forKey: "incomes") as? [[String:String]] {
+            incomes = temp
+        }
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let temp = UserDefaults.standard.object(forKey: "incomes") as? [[String:String]] {
+            incomes = temp
+        }
+        
+        var total = 0.0
+        for income in incomes {
+            if let amount = income["amount"] {
+                total += Double(amount)!
+            }
+        }
+        totalLabel.text = "+\(total)$"
+        
+        table.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return incomes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = table.dequeueReusableCell(withIdentifier: "RecordCell", for: indexPath) as? RecordTableViewCell {
+            cell.reasonLabel.text = incomes[indexPath.row]["reason"]
+            cell.dateLabel.text = incomes[indexPath.row]["date"]
+            cell.amountLabel.text = incomes[indexPath.row]["amount"]
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
     @IBAction func addButtonClicked(_ sender: Any) {
         self.performSegue(withIdentifier: "addIncomeSegue", sender: nil)
     }
--- Track My Finance/NewRecordViewController.swift
+++ Track My Finance/NewRecordViewController.swift
@@ -11,14 +11,53 @@ import UIKit
 class NewRecordViewController: UIViewController {
     
     @IBOutlet weak var navTitle: UINavigationItem!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var reasonTextField: UITextField!
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    var dataStored = [[String:String]]()
+
+    @IBAction func addButtonClicked(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        let date = dateFormatter.string(from: datePicker.date)
+        
+        if reasonTextField.text != "" && amountTextField.text != "" {
+            if let reason = reasonTextField.text {
+                if let amount = amountTextField.text {
+                    let newIncome = ["date":date, "reason":reason, "amount": amount]
+                    dataStored.append(newIncome)
+                    
+                    // SAVE DATA
+                    if state == "income" {
+                        UserDefaults.standard.set(dataStored, forKey: "incomes")
+                    } else {
+                        UserDefaults.standard.set(dataStored, forKey: "outgoings")
+                    }
+                }
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if state == "income" {
             navTitle.title = "Add new income"
+            reasonTextField.placeholder = "E.g. Salary"
+            amountTextField.placeholder = "345.9"
+            if let temp = UserDefaults.standard.object(forKey: "incomes") as? [[String:String]] {
+                dataStored = temp
+            }
         } else {
             navTitle.title = "Add new outgoing"
+            reasonTextField.placeholder = "E.g. Friday night"
+            amountTextField.placeholder = "19.9"
+            if let temp = UserDefaults.standard.object(forKey: "outgoings") as? [[String:String]] {
+                dataStored = temp
+            }
         }
     }
 
